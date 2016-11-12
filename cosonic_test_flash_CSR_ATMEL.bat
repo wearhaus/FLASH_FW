05d2fb54592a9426eeefcce342fd0a6ae7189018
nelsonz <nelsonz@acm.org>
2016-11-12 02:26:01 -0800

    ...i'm an idiot

--- cosonic_test_flash_CSR_ATMEL.bat
+++ cosonic_test_flash_CSR_ATMEL.bat
@@ -3,6 +3,10 @@
 
 :start
 
+set atmel_path= "C:\Program Files (x86)\Atmel\Studio\7.0"
+set csr_path= "C:\ADK3.5\tools\bin"
+::set csr_path= "C:\Program Files (x86)\CSR\BlueSuite 2.6.2"
+
 pause
 
 echo.
@@ -71,21 +75,21 @@ echo.
 
 echo ******************** START **************************
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz chiperase
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz chiperase
 
 
 echo. 
 echo MTCH6301:
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -ee --format hex -f "wear - MTCH_SET.eep"
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -ee --format hex -f "wear - MTCH_SET.eep"
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -fl --format hex -f "wear - MTCH_SET.elf"
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -fl --format hex -f "wear - MTCH_SET_3.elf"
 
 if %ERRORLEVEL% == 0 (
 	echo MTCH6301 PASS
 ) ELSE (
 	echo MTCH6301 FAIL
-	msg /time:5 "%username%" B
+	msg /time:5 "%username%" FAIL
 	goto fail
 )
 
@@ -94,14 +98,14 @@ if %ERRORLEVEL% == 0 (
 echo. 
 echo FLASH:
 
-"C:\ADK3.5\tools\bin\nvscmd" -usb 0 erase
-"C:\ADK3.5\tools\bin\nvscmd" -usb 0 burn sqif2.ptn all
+::call %csr_path%\nvscmd -usb 0 erase
+::call %csr_path%\nvscmd -usb 0 burn sqif2.ptn all
 
 if %ERRORLEVEL% == 0 (
 	echo FLASH PASS
 ) ELSE (
 	echo FLASH FAIL
-	msg /time:5 "%username%" B
+	msg /time:5 "%username%" FAIL
 	goto fail
 )
 
@@ -110,33 +114,35 @@ if %ERRORLEVEL% == 0 (
 echo. 
 echo CSR:
 
-C:\ADK3.5\tools\bin\BlueFlashCmd dump-NELSON
+call %csr_path%\BlueFlashCmd 20161108-dump--mictest
 
 
 if %ERRORLEVEL% == 0 (
 	echo CSR PASS
 ) ELSE (
 	echo CSR FAIL
-	msg /time:5 "%username%" B
+	msg /time:5 "%username%" FAIL
 	goto fail
 )
 
-
+echo PRESS ANY KEY WHEN LEDS ARE FLASHING RAINBOW/GREEN. IF FLASHING RED, OPEN AND CLOSE TEST MACHINE.
+pause
 
 echo. 
 echo ATMEL:
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -ee --format hex -f "wear - MTCH_SET.eep"
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz chiperase
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -fl --format hex -f "wear - 11-11-2016.elf"
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -ee --format hex -f "wear - MTCH_SET.eep"
 
-"C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\atprogram" -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz reset
+call %atmel_path%\atbackend\atprogram -t atmelice -i PDI -d ATXMEGA32E5 -cl 7500khz program -fl --format hex -f "wear - nelson4.hex"
+
 
 if %ERRORLEVEL% == 0 (
 	echo ATMEL PASS
 ) ELSE (
 	echo ATMEL FAIL
-	msg /time:5 "%username%" B
+	msg /time:5 "%username%" FAIL
 	goto fail
 )
 
@@ -169,10 +175,10 @@ set hexwords=%hexwords:~2,2%%hexwords:~0,2% %hexwords:~6,2%%hexwords:~4,2% %hexw
 
 >setname.psr echo ^&0108 = %wearhausarc% %hexwords%
 
-C:\ADK3.5\tools\bin\pscli -usb 0 merge setaddr.psr
-C:\ADK3.5\tools\bin\pscli -usb 0 merge setname.psr
-C:\ADK3.5\tools\bin\pscli -usb 0 merge setdefaulttricolor.psr
-C:\ADK3.5\tools\bin\pscli -usb 0 warm_reset
+call %csr_path%\pscli -usb 0 merge setaddr.psr
+call %csr_path%\pscli -usb 0 merge setname.psr
+call %csr_path%\pscli -usb 0 merge setdefaulttricolor.psr
+call %csr_path%\pscli -usb 0 warm_reset
 
 echo .
 echo ADDRESS: %byte01% %byte2% %byte3% %byte45%
@@ -182,7 +188,7 @@ if %ERRORLEVEL% == 0 (
 	echo ADDR PASS
 ) ELSE (
 	echo ADDR FAIL
-	msg /time:5 "%username%" B
+	msg /time:5 "%username%" FAIL
 	goto fail
 )
 
@@ -250,28 +256,36 @@ echo.
 echo.
 echo.
 echo.
-echo ````###````
-echo ```##`##```
-echo ``##```##``
-echo `##`````##`
-echo `#########`
-echo `##`````##`
-echo `##`````##`
+echo `##########```##``````##`
+echo `##``````##```##````##```
+echo `##``````##```##``##`````
+echo `##``````##```####```````
+echo `##``````##```##``##`````
+echo `##``````##```##````##```
+echo `##########```##``````##`
 echo.
 
-msg /time:5 "%username%" A
+msg /time:5 "%username%" OK
 
 goto start
 
 
 :fail
 
-echo `########``
-echo `##`````##`
-echo `##`````##`
-echo `########``
-echo `##`````##`
-echo `##`````##`
-echo `########``
+::echo `##``````##```##########`
+::echo `####````##```##`````````
+::echo `##`##```##```##`````````
+::echo `##``##``##```##````####`
+::echo `##```##`##```##``````##`
+::echo `##````####```##``````##`
+::echo `##``````##```##########`
+echo `##########````###``````##```##`````````
+echo `##```````````##`##`````##```##`````````
+echo `##``````````##```##````##```##`````````
+echo `#########``##`````##```##```##`````````
+echo `##`````````#########```##```##`````````
+echo `##`````````##`````##```##```##`````````
+echo `##`````````##`````##```##```##########`
+echo.
 
 goto start
